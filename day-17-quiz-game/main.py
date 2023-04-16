af68278ba905a1a8f09f2fb40d70ce272f27684e
Danny Chan <dannychan0510@gmail.com>
2023-04-16 12:10:09 +0900

    Complete day 17 quiz game project.

--- day-17-quiz-game/main.py
+++ day-17-quiz-game/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 # Initialise empty question bank
 question_bank = []
@@ -8,3 +9,13 @@ question_bank = []
 for q in question_data:
     question_bank.append(Question(q['text'], q['answer']))
 
+# Create QuizBrain object
+quiz = QuizBrain(question_bank)
+
+# Continue quiz as long as quiz still has questions
+while quiz.still_has_questions():
+    quiz.next_question()
+
+# Show final message and score once all questions are asked
+print(f"You've completed the quiz!")
+print(f"You're final score was: {quiz.score}/{quiz.question_number}")
--- day-17-quiz-game/quiz_brain.py
+++ day-17-quiz-game/quiz_brain.py
@@ -4,8 +4,46 @@
 
 class QuizBrain():
 
-    def __init__(self):
+    def __init__(self, question_list):
         self.question_number = 0
-        self.questions_list
+        self.score = 0
+        self.question_list = question_list
+
+
+    def still_has_questions(self):
+        """
+        Returns True if quiz still has questions in question_list to ask user
+        """
+        return len(self.question_list) > self.question_number
+
 
     def next_question(self):
+        """
+        Retrieves the next question from the question_list, increments the question_number by 1,
+        shows the question to the user, and waits for the user's response.
+        """
+        # Retrieve the item at the current question_number from the question_list
+        question = self.question_list[self.question_number]
+
+        # Increment question number by 1
+        self.question_number += 1
+
+        # User the input() function to show the user the q text and ask for the user's answer
+        user_answer = input(f"Q.{self.question_number}: {question.text} (True/False): ")
+
+        # Check answer
+        self.check_answer(user_answer, question.answer)
+
+
+    def check_answer(self, user_answer, correct_answer):
+        """
+        Checks if the user's answer is correct and updates the score.
+        """
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That is wrong.")
+        print(f"The correct answer was: {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")

7f6592b7d0c8e2e8c810789ebdd4231a08aea459
Danny Chan <dannychan0510@gmail.com>
2023-04-28 23:05:55 +0800

    Create Snake class in separate snake.py

--- day-20-21-snake-game/main.py
+++ day-20-21-snake-game/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 # Setting up the screen
@@ -9,23 +10,15 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 # Creating a snake body
-segments = []
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
-
-for position in starting_position:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.setposition(position)
-    segments.append(new_segment)
+snake = Snake()
 
 game_is_on = True
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
+
+    snake.move()
 
 # Setting screen to exit on click only
 screen.exitonclick()

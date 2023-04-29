9c2a644549f551e7d39a7b034c016945db3e0bc7
Danny Chan <dannychan0510@gmail.com>
2023-04-30 01:34:16 +0800

    Add wall and paddle collision logic.

--- day-22-pong/main.py
+++ day-22-pong/main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 # Set up the screen
 screen = Screen()
@@ -12,6 +14,8 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
+# Create a ball
+ball = Ball()
 
 # Listen for keyboard input
 screen.listen()
@@ -22,12 +26,21 @@ screen.onkeypress(r_paddle.down, "Down")
 screen.onkeypress(l_paddle.up, "q")
 screen.onkeypress(l_paddle.down, "a")
 
-
 # Initialize game loop
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect collision with top and bottom walls
+    if abs(ball.ycor()) > 280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.collide_with_paddle(l_paddle) or ball.collide_with_paddle(r_paddle):
+        ball.bounce_x()
 
 # Set screen to exit only on click
 screen.exitonclick()

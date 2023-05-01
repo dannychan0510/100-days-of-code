3586bbacc586131d37e83479fefaac3f79a3f318
Danny Chan <dannychan0510@gmail.com>
2023-05-02 01:49:31 +0800

    Complete day 24 Mail Merge Challenge.

---
+++ day-24-mail-merge-challenge/README.md
@@ -0,0 +1,18 @@
+# Day 24: Mail Merge Challenge
+
+This script will read a list of names and addresses from a file, and then create a personalized letter for each person. The letters will be saved to a separate directory.
+
+## How to use
+
+1. Create a directory called `Input` and a directory called `Output`.
+2. In the `Input` directory, create a file called `names.txt` and a file called `starting_letter.txt`.
+3. In the `names.txt` file, enter a list of names, one per line.
+4. In the `starting_letter.txt` file, enter the text of the starting letter. Add `[name]` as a placeholder for the name of the recipient.
+5. Run the script by typing the following command in the terminal:
+
+## Key concepts learned
+
+* How to read and write files in Python
+* How to access directories in Python
+* How to use string formatting to personalize a letter
+
--- day-24-mail-merge-challenge/main.py
+++ day-24-mail-merge-challenge/main.py
@@ -1,8 +1,28 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# Open the file "Input/Letters/starting_letter.txt" in read mode.
+# The variable `starting_letter` will contain the contents of the file as a list of lines.
+with open("Input/Letters/starting_letter.txt", mode="r") as f:
+    starting_letter = f.readlines()
+
+# Open the file "Input/Names/invited_names.txt" in read mode.
+# The variable `invited_names` will contain the contents of the file as a list of lines.
+with open("Input/Names/invited_names.txt", mode="r") as f:
+    invited_names = f.readlines()
+
+# Iterate over the list of invited names.
+for name in invited_names:
+
+    # Remove the newline character from the end of the name.
+    name = name.strip("\n")
+
+    # Open a new file called "Output/ReadyToSend/letter_to_" + name + ".txt" in write mode.
+    # The variable `f` will refer to the file object.
+    with open(f"Output/ReadyToSend/letter_to_{name}.txt", mode="w") as f:
+
+        # Iterate over the list of lines in the starting letter.
+        for line in starting_letter:
+
+            # Replace the placeholder "[name]" with the current name.
+            new_line = line.replace("[name]", name)
+
+            # Write the new line to the file.
+            f.write(new_line)

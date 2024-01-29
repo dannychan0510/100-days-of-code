17871b26eb208ce8d3531caddd990a5861af67c1
Danny Chan <dannychan0510@gmail.com>
2024-01-29 22:17:50 +0900

    Improved comments

--- day-26-nato-alphabet/main.py
+++ day-26-nato-alphabet/main.py
@@ -1,25 +1,32 @@
+# Import libraries
 import pandas
 
-# Read the NATO phonetic alphabet data from a CSV file
+# Reading the NATO phonetic alphabet data from a CSV file and storing it in a DataFrame
 nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-# Create a dictionary with letters as keys and their corresponding NATO phonetic codes as values
+# Creating a dictionary comprehension to map each letter to its corresponding NATO phonetic code.
+# The dictionary uses letters as keys and their corresponding NATO phonetic codes as values.
 nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
 
 
-# Get user input, and convert to upper case
+# Defining a function to convert user input into its equivalent NATO phonetic alphabet codes
 def generate_phonetic():
+    # Prompting the user to enter a word and converting the input to uppercase to match dictionary keys
     user_input = input("Enter a word: ").upper()
 
-    # Convert the user input into a list of NATO phonetic alphabet codes
+    # Attempting to map each letter of the user input to its NATO phonetic code
     try:
+        # Using list comprehension to create a list of NATO phonetic codes corresponding to the user's input
         user_input_nato_alphabet = [nato_alphabet_dict[letter] for letter in user_input]
     except KeyError:
+        # Handling the case where the user input contains characters not found in the NATO alphabet dictionary
         print('Sorry, only letters in the alphabet please.')
+        # Recursively calling the function to prompt the user for another input
         generate_phonetic()
     else:
-        # Print the NATO phonetic alphabet codes for the user input
+        # Displaying the NATO phonetic codes for the user's input
         print(user_input_nato_alphabet)
 
 
+# Calling the function to start the program
 generate_phonetic()

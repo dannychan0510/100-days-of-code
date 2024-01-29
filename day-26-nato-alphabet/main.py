5869231ab8ba96e0d5732185f9240e0c1e0d0dcd
Danny Chan <dannychan0510@gmail.com>
2024-01-29 22:14:05 +0900

    Added exception catching (based on Day 31 learnings)

--- day-26-nato-alphabet/main.py
+++ day-26-nato-alphabet/main.py
@@ -6,11 +6,20 @@ nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 # Create a dictionary with letters as keys and their corresponding NATO phonetic codes as values
 nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
 
+
 # Get user input, and convert to upper case
-user_input = input("Enter a word: ").upper()
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
 
-# Convert the user input into a list of NATO phonetic alphabet codes
-user_input_nato_alphabet = [nThaato_alphabet_dict[letter] for letter in user_input]
+    # Convert the user input into a list of NATO phonetic alphabet codes
+    try:
+        user_input_nato_alphabet = [nato_alphabet_dict[letter] for letter in user_input]
+    except KeyError:
+        print('Sorry, only letters in the alphabet please.')
+        generate_phonetic()
+    else:
+        # Print the NATO phonetic alphabet codes for the user input
+        print(user_input_nato_alphabet)
 
-# Print the NATO phonetic alphabet codes for the user input
-print(user_input_nato_alphabet)
+
+generate_phonetic()

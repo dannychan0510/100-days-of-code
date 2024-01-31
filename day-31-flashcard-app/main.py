daafccc0905be0bcd3af2c9ec87d4da9077b8058
Danny Chan <dannychan0510@gmail.com>
2024-02-01 01:57:57 +0900

    Add basic functionality to go to next card to buttons

--- day-31-flashcard-app/main.py
+++ day-31-flashcard-app/main.py
@@ -1,6 +1,22 @@
 import tkinter as tk
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+# Read data
+words = pd.read_csv('data/french_words.csv')
+words = words.to_dict(orient='records')
+
+
+# ---------------------------- FUNCTIONS ------------------------------- #
+# Get new random word
+def next_card():
+    current_card = random.choice(words)
+    french_word = current_card['French']
+    english_word = current_card['English']
+    canvas.itemconfig(card_word, text=french_word)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Initialise window
@@ -15,20 +31,21 @@ right_img = tk.PhotoImage(file='images/right.png')
 
 # Create canvas
 canvas = tk.Canvas(width=800, height=526)
-canvas.create_image(400, 263,image=card_front_img)
+canvas.create_image(400, 263, image=card_front_img)
 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
 # Add text to canvas
-canvas.create_text(400, 150, text='Title', font=('Arial', 40, 'italic'))
-canvas.create_text(400, 263, text='Word', font=('Arial', 60, 'bold'))
+card_title = canvas.create_text(400, 150, text='French', font=('Arial', 40, 'italic'))
+card_word = canvas.create_text(400, 263, text='Word', font=('Arial', 60, 'bold'))
 
 # Add buttons
-wrong_button = tk.Button(image=wrong_img, highlightthickness=0)
-right_button = tk.Button(image=right_img, highlightthickness=0)
+wrong_button = tk.Button(image=wrong_img, highlightthickness=0, command=next_card)
+right_button = tk.Button(image=right_img, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
 right_button.grid(row=1, column=1)
 
-# Initialise mainloop()
+# Initialise first card, and start mainloop()
+next_card()
 window.mainloop()
